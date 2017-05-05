5396b4fa5f6baf623e3fd0f1147bb62c224f900f
dbaonguis <dbaonguis@yahoo.com>
2017-05-04 23:50:44 -0400

    - Change data - Add "dev" profile, and set it as "default" profile

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,7 +1,7 @@
 DELETE FROM `superhero`;
 INSERT INTO `superhero` (`name`, `first_name`, `last_name`, `good`) VALUES
-  ('Superman', 'Clark', 'Kent', 1),
-  ('Silver Banshee', 'Siobhan', 'McDougal', 1),
-  ('Super Don', 'Donato', 'Baonguis', 1),
-  ('Mighty HW', 'Hello', 'World', 1),
-  ('Captain America', 'Steve', 'Rogers', 1);
+  ('Superman', 'Clark', 'Kent', 0),
+  ('Silver Banshee', 'Siobhan', 'McDougal', 0),
+  ('Super Don', 'Donato', 'Baonguis', 0),
+  ('Mighty HW', 'Hello', 'World', 0),
+  ('Captain America', 'Steve', 'Rogers', 0);
